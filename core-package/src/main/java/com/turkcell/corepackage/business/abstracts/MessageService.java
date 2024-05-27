e8a19a2819a1d37a0136a50fa9a950b741c87fdc
Yasin <yasinzc75@gmail.com>
2024-05-27 13:12:46 +0300

    publish core-package

--- core-package/src/main/java/com/turkcell/corepackage/business/abstracts/MessageService.java
+++
@@ -1,8 +0,0 @@
-package com.turkcell.corepackage.business.abstracts;
-
-public interface MessageService {
-    String getMessage(String key);
-    String getMessageWithArgs(String key, Object... args);
-}
-
-
