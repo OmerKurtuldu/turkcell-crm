9407cdbfff1826b025ca4b13bbf77b5054409ca3
Yasin <yasinzc75@gmail.com>
2024-05-30 23:32:54 +0300

    added businessRules for orderService

---
+++ orderService/src/main/java/com/turkcell/orderService/business/messages/Messages.java
@@ -0,0 +1,9 @@
+package com.turkcell.orderService.business.messages;
+
+public class Messages {
+    public static class OrderErrors{
+        public static final String BasketRegistrationShouldBeExist = "basketRegistrationShouldBeExist";
+        public static final String OrderShouldBeExist = "orderShouldBeExist";
+
+    }
+}
